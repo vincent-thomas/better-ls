4745de8f82a32944fe1cca66a72a98e01d128e19
Vincent Thomas <vincent.nathan.thomas@gmail.com>
2022-06-19 23:53:35 +0200

    feat: better visuals in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use comfy_table::Table;
+use std::fs::read_dir;
+use std::io::{stderr, Write};
+use std::{env, process};
+
+struct File {
+    name: String,
+    is_dir: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let dir = match args.len() {
+        1 => ".".to_string(),
+        2 => args[1].to_string(),
+        _ => {
+            stderr().write_all(b"Too many arguments\n").unwrap();
+            process::exit(1);
+        }
+    };
+    let result = read_dir(dir).unwrap();
+
+    let mut table = Table::new();
+
+    for entry in result {
+        let value = &entry.unwrap();
+        let file = File {
+            name: value.file_name().to_str().unwrap().to_string(),
+            is_dir: value.path().is_dir(),
+        };
+        table.add_row([&file.name, &file.is_dir.to_string()]);
+    }
+    println!("{table}")
 }
